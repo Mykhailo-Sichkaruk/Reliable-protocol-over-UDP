d31d55f0cc750e0ec828e2e930cb4d1daf0c59d0
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-12-11 16:23:52 +0100

    Small fixes

--- src/initData.py
+++ src/initData.py
@@ -2,26 +2,24 @@ import hashlib
 import logging
 import os
 
-from services import time_ms
 
-
-log = logging.getLogger("server")
+log = logging.getLogger(__name__)
 
 
 class InitData:
     """Class for creating the init data for the file transfer
-    file_len: 8B, Length of the file in bytes
-    md5_hash: 16B, MD5 hash of the file
-    file_path: 0-255B, Path of the file
+
+    file_len:  8B,      Length of the file in bytes
+    md5_hash:  16B,     MD5 hash of the file
+    file_path: 0-255B   Path of the file
     """
 
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
-        self.len: int = 0
+        self.__len: int = 0
         # Try to open the file to check if it exists
         try:
             self.file = open(file_path, "rb")
-            log.warn(f"File {file_path} opened, time: {time_ms()}")
             # Get the length of the file
             self.file_len = os.path.getsize(file_path)
             # Calculate md5 hash of the file
@@ -33,27 +31,26 @@ class InitData:
             raise FileNotFoundError("File does not exist")
 
     @property
+    def md5_hash(self) -> bytes:
+        return self.hash
+
+    @property
     def bytes(self) -> bytes:
         data = b''
         data += self.file_length.to_bytes(8, "big")
         data += self.hash
         data += bytes(self.file_path, "utf-8")
 
-        self.len = len(data)
+        self.__len = len(data)
         return data
-
-    @property
-    def md5_hash(self) -> bytes:
-        return self.hash
 
     @property
     def file_len(self) -> int:
         return self.file_length
 
-    # Add setter for file_len
     @file_len.setter
     def file_len(self, value: int) -> None:
         self.file_length = value
 
     def __len__(self) -> int:
-        return self.len
+        return self.__len
