36b74fb4084d1a90a1e79ec4a94b476375c0b964
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-12-12 01:34:55 +0100

    Add main log file and md5_file()

--- src/services.py
+++ src/services.py
@@ -1,5 +1,5 @@
-from email import message
-from colorlog import ColoredFormatter
+import hashlib
+import colorlog
 from time import time
 
 MSG_SEND: str = "./src/__pycache__/.msg"
@@ -10,9 +10,18 @@ def time_ms() -> int:
     return int(time() * 1000)
 
 
-formatter: ColoredFormatter = ColoredFormatter(
-    fmt="%(log_color)s%(levelname)-8s%(reset)s %(message_log_color)s%(message)s",
-    datefmt=None,
+def md5_file(file_path: str) -> str:
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
+
+
+formatter: colorlog.ColoredFormatter = colorlog.ColoredFormatter(
     log_colors={
         'DEBUG':    'white',
         'INFO':     'cyan',
@@ -31,9 +40,15 @@ formatter: ColoredFormatter = ColoredFormatter(
 
     },
     style='%',
-    force_color=True,
 )
+handler = colorlog.StreamHandler()
+handler.setFormatter(formatter)
 
+colorlog.getLogger('').addHandler(handler)
+global log
+log = colorlog.getLogger(__name__)
+log.setLevel('DEBUG')
+log.info("Logger initialized")
 """_summary_escape_codes = {
     "reset": esc(0),
     "bold": esc(1),
