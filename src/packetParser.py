53ab327c245e64efb33cb2731cbd0a4073ca003a
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2023-01-29 21:49:31 +0100

    Rewrite packet header - 4B for window number

--- src/packetParser.py
+++ src/packetParser.py
@@ -4,6 +4,16 @@ from dataclasses import dataclass
 from enum import Enum
 
 
+FLAGS_BYTES: int = 1
+"""Number of bytes for flags"""
+PACKET_NUMBER_B: int = 1
+"""Number of bytes for packet number"""
+WINDOW_SIZE_B: int = 4
+"""Number of bytes for window size"""
+MAX_WINDOW_NUMBER: int = 2 ** (WINDOW_SIZE_B * 8) - 1
+"""Maximum number of the window"""
+
+
 class PacketType(Enum):
     Data = 1
     Confirm = 2
@@ -27,42 +37,47 @@ class MRP:
     @staticmethod
     def deserialize(data: bytes):
         """Parse bytes into MRP"""
-        packet_type, file_id = MRP.parse_first_byte(data[0])
-        packet_number_in_window = data[1]
-        window_number = int.from_bytes(data[2:4], "big")
-        payload = data[8:]
+        flags = data[0:(FLAGS_BYTES)]
+        packet_type, file_id = MRP.parse_flags(flags)
+        packet_number: int = int.from_bytes(data[FLAGS_BYTES:(FLAGS_BYTES +
+                                                              PACKET_NUMBER_B)], "big")
+        window_number = int.from_bytes(
+            data[(FLAGS_BYTES+PACKET_NUMBER_B):+(FLAGS_BYTES+PACKET_NUMBER_B+WINDOW_SIZE_B)], "big")
+        payload = data[(FLAGS_BYTES+PACKET_NUMBER_B +
+                        WINDOW_SIZE_B):]
 
-        return MRP(packet_type, file_id, packet_number_in_window, window_number, payload)
+        return MRP(packet_type, file_id, packet_number, window_number, payload)
 
     @staticmethod
-    def parse_first_byte(data: int) -> tuple[PacketType, int]:
-        packet_type_number = data >> 4
+    def parse_flags(data: bytes) -> tuple[PacketType, int]:
+        flags = int.from_bytes(data, "big")
+        packet_type_number = flags >> 4
         if packet_type_number < 0 or packet_type_number > 12:
             packet_type_number = 0
         packet_type = PacketType(packet_type_number)
 
-        file_id = data & 0b00001111
+        file_id = flags & 0b00001111
 
         return packet_type, file_id
 
-    @staticmethod
+    @ staticmethod
     def check_checksum(data: bytes) -> bool:
         """Check if checksum is correct"""
         checksum = int.from_bytes(data[4:8], "big")
         return checksum == crc32(data[0:4] + data[8:])
 
-    @staticmethod
+    @ staticmethod
     def serialize(type: PacketType, file_id: int, number_in_window: int, window_number: int, payload: bytes) -> bytes:
         """Create MRP packet"""
-        first_byte = (type.value << 4) + file_id
-        packet_number_in_window = number_in_window.to_bytes(1, "big")
-        packet_window_number = window_number.to_bytes(2, "big")
-        checksum = crc32(bytes([first_byte]) + packet_number_in_window +
-                         packet_window_number + payload).to_bytes(4, "big")
+        first_byte: int = (type.value << 4) + file_id
+        packet_number_in_window = number_in_window.to_bytes(
+            PACKET_NUMBER_B, "big")
+        packet_window_number = window_number.to_bytes(
+            WINDOW_SIZE_B, "big")
 
-        return bytes([first_byte]) + packet_number_in_window + packet_window_number + checksum + payload
+        return bytes([first_byte]) + packet_number_in_window + packet_window_number + payload
 
-    @staticmethod
+    @ staticmethod
     def broke_packet(data: bytes):
         # Change any byte in the packet
         random_index = randint(0, len(data) - 1)
